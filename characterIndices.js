f001c26204a4daa073b9d6e271e10f7d5a757517
Shay Alexander <shaytopazehair@gmail.com>
2018-01-18 17:00:22 +0000

    Refactored Code

--- characterIndices.js
+++ characterIndices.js
@@ -1,37 +1,29 @@
-function characterPosition(inputString) {
-var intermediateString = inputString.join(""); //removes spaces lighthouselabs
-var inputArray = intermediateString.split(""); //split individual characters into array
-var outputObject = {};
-var letterStore = []; //stores an array of the keys of outputObject
+function countLetters(inputString) {
 
-for (var i = 0; i < inputArray.length; i++){
-  if(isLetterInside(inputArray[i], letterStore)) {
-    outputObject[inputArray[i]].push(i);
+  var intermediateString = inputString.replace(/ /g, ''); //removes spaces lighthouselabs
+  var inputArray = intermediateString.split(""); //split individual characters into array
+  var outputObject = {};
+  // var letterStore = []; //stores an array of the keys of outputObject`
+  for (var i = 0; i < inputArray.length; i++){
+
+    var letter = inputArray[i];
+
+    outputObject[letter] = [];
+
+    if(outputObject[letter]) {
+      // if letter has not been encountered before
+      outputObject[letter].push([i]);
+
+    } else {
+      outputObject[letter]= (i);
+    }
+
   }
-  // if letter has been encountered before
 
-  else {
-    // if letter has not been encountered before
-    outputObject[inputArray[i]] = [i];
-  }
-
-  letterStore = Object.keys(outputObject); //stores an array of the keys of outputObject
+  return outputObject;
 
 }
 
-return outputObject;
+var args = process.argv[2];
 
-  function isLetterInside(letter, array) {
-    for (var j = 0; j < array.length; j++) {
-      if (array[j] == letter) {
-        return true;
-       }
-     }
-     return false;
-  }
-
-}
-
-
-args = process.argv.slice(2);
-console.log(characterPosition(args));
+console.log(countLetters(args));
